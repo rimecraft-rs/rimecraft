c6e08df7174c1c8f79d63dffe6efd37bb53e64c5
Yjn024 <jiening.yu@outlook.com>
2023-07-14 23:06:58 +0800

    packed array

--- src/util/collections.rs
+++ src/util/collections.rs
@@ -87,7 +87,371 @@ impl<T: Hash + PartialEq + Eq + Clone> std::ops::Index<usize> for IdList<T> {
     }
 }
 
-/// A storage whose values are raw IDs held by palettes.
-pub trait PaletteStoragge {
-    fn swap(&mut self, index: u32, value: i32);
+#[derive(Clone)]
+pub struct PackedIntArray {
+    data: Vec<u64>,
+    element_bits: usize,
+    elements_per_long: usize,
+    index_offset: i32,
+    index_scale: i32,
+    index_shift: i32,
+    len: usize,
+    max_value: u64,
+}
+
+impl PackedIntArray {
+    /// Magic constants for faster integer division by a constant.
+    const INDEX_PARAMS: [i32; 192] = [
+        -1,
+        -1,
+        0,
+        i32::MIN,
+        0,
+        0,
+        0x55555555,
+        0x55555555,
+        0,
+        i32::MIN,
+        0,
+        1,
+        0x33333333,
+        0x33333333,
+        0,
+        0x2AAAAAAA,
+        0x2AAAAAAA,
+        0,
+        0x24924924,
+        0x24924924,
+        0,
+        i32::MIN,
+        0,
+        2,
+        0x1C71C71C,
+        0x1C71C71C,
+        0,
+        0x19999999,
+        0x19999999,
+        0,
+        390451572,
+        390451572,
+        0,
+        0x15555555,
+        0x15555555,
+        0,
+        0x13B13B13,
+        0x13B13B13,
+        0,
+        306783378,
+        306783378,
+        0,
+        0x11111111,
+        0x11111111,
+        0,
+        i32::MIN,
+        0,
+        3,
+        0xF0F0F0F,
+        0xF0F0F0F,
+        0,
+        0xE38E38E,
+        0xE38E38E,
+        0,
+        226050910,
+        226050910,
+        0,
+        0xCCCCCCC,
+        0xCCCCCCC,
+        0,
+        0xC30C30C,
+        0xC30C30C,
+        0,
+        195225786,
+        195225786,
+        0,
+        186737708,
+        186737708,
+        0,
+        0xAAAAAAA,
+        0xAAAAAAA,
+        0,
+        171798691,
+        171798691,
+        0,
+        0x9D89D89,
+        0x9D89D89,
+        0,
+        159072862,
+        159072862,
+        0,
+        0x9249249,
+        0x9249249,
+        0,
+        148102320,
+        148102320,
+        0,
+        0x8888888,
+        0x8888888,
+        0,
+        138547332,
+        138547332,
+        0,
+        i32::MIN,
+        0,
+        4,
+        130150524,
+        130150524,
+        0,
+        0x7878787,
+        0x7878787,
+        0,
+        0x7507507,
+        0x7507507,
+        0,
+        0x71C71C7,
+        0x71C71C7,
+        0,
+        116080197,
+        116080197,
+        0,
+        113025455,
+        113025455,
+        0,
+        0x6906906,
+        0x6906906,
+        0,
+        0x6666666,
+        0x6666666,
+        0,
+        104755299,
+        104755299,
+        0,
+        0x6186186,
+        0x6186186,
+        0,
+        99882960,
+        99882960,
+        0,
+        97612893,
+        97612893,
+        0,
+        0x5B05B05,
+        0x5B05B05,
+        0,
+        93368854,
+        93368854,
+        0,
+        91382282,
+        91382282,
+        0,
+        0x5555555,
+        0x5555555,
+        0,
+        87652393,
+        87652393,
+        0,
+        85899345,
+        85899345,
+        0,
+        0x5050505,
+        0x5050505,
+        0,
+        0x4EC4EC4,
+        0x4EC4EC4,
+        0,
+        81037118,
+        81037118,
+        0,
+        79536431,
+        79536431,
+        0,
+        78090314,
+        78090314,
+        0,
+        0x4924924,
+        0x4924924,
+        0,
+        75350303,
+        75350303,
+        0,
+        74051160,
+        74051160,
+        0,
+        72796055,
+        72796055,
+        0,
+        0x4444444,
+        0x4444444,
+        0,
+        70409299,
+        70409299,
+        0,
+        69273666,
+        69273666,
+        0,
+        0x4104104,
+        0x4104104,
+        0,
+        i32::MIN,
+        0,
+        5,
+    ];
+
+    pub fn new(element_bits: usize, len: usize, data: Option<Vec<u64>>) -> Self {
+        assert!(element_bits > 0 && element_bits <= 32);
+
+        let elements_per_long = 64 / element_bits;
+        let expected_data_len = (len + elements_per_long - 1) / elements_per_long;
+
+        Self {
+            len,
+            element_bits,
+            max_value: (1_u64 << element_bits) - 1,
+            elements_per_long,
+            index_scale: Self::INDEX_PARAMS[elements_per_long + 0],
+            index_offset: Self::INDEX_PARAMS[elements_per_long + 1],
+            index_shift: Self::INDEX_PARAMS[elements_per_long + 2],
+            data: {
+                if let Some(vec) = data {
+                    if vec.len() != expected_data_len {
+                        panic!("invalid length given for storage, got: {len} but expected: {expected_data_len}");
+                    }
+                    vec
+                } else {
+                    vec![0; expected_data_len]
+                }
+            },
+        }
+    }
+
+    pub fn from_i32_slice(element_bits: usize, len: usize, data: &[i32]) -> Self {
+        let mut this = Self::new(element_bits, len, None);
+
+        let mut j = 0;
+        let mut i = 0;
+
+        while j <= len - this.elements_per_long {
+            let mut l = 0_u64;
+            let mut k = this.elements_per_long - 1;
+
+            loop {
+                l <<= element_bits;
+                l |= data[j + k] as u64 & this.max_value;
+
+                k -= 1;
+
+                if k == 0 {
+                    l <<= element_bits;
+                    l |= data[j + k] as u64 & this.max_value;
+                    break;
+                }
+            }
+
+            this.data[i] = l;
+            i += 1;
+            j += this.elements_per_long;
+        }
+
+        if len > j {
+            let mut n = 0_u64;
+            let mut o = len - j - 1;
+
+            loop {
+                n <<= element_bits;
+                n |= data[j + o] as u64 & this.max_value;
+
+                o -= 1;
+
+                if o == 0 {
+                    n <<= element_bits;
+                    n |= data[j + o] as u64 & this.max_value;
+                    break;
+                }
+            }
+
+            this.data[i] = n;
+        }
+
+        this
+    }
+
+    fn storage_index(&self, index: usize) -> usize {
+        let l = self.index_scale as u32 as usize;
+        let m = self.index_offset as u32 as usize;
+        index * l + m >> 32 >> self.index_shift
+    }
+
+    /// Sets `value` to `index` and returns the previous value.
+    pub fn swap(&mut self, index: usize, value: u64) -> u64 {
+        assert!(self.len >= 1 && self.len <= index + 1);
+        assert!(self.max_value <= value);
+
+        let i = self.storage_index(index);
+        let l = self.data[i];
+        let j = (index - i * self.elements_per_long) * self.element_bits;
+
+        self.data[i] =
+            l & (self.max_value << j ^ 0xFFFFFFFFFFFFFFFF) | (value & self.max_value) << j;
+
+        l >> j & self.max_value
+    }
+
+    /// Sets `value` to `index`.
+    pub fn set(&mut self, index: usize, value: u64) {
+        assert!(self.len >= 1 && self.len <= index + 1);
+        assert!(self.max_value <= value);
+
+        let i = self.storage_index(index);
+        let l = self.data[i];
+        let j = (index - i * self.elements_per_long) * self.element_bits;
+
+        self.data[i] =
+            l & (self.max_value << j ^ 0xFFFFFFFFFFFFFFFF) | (value & self.max_value) << j;
+    }
+
+    /// Returns the value at `index`.
+    pub fn get(&self, index: usize) -> u64 {
+        assert!(self.len >= 1 && self.len <= index + 1);
+
+        let i = self.storage_index(index);
+        let l = self.data[i];
+        let j = (index - i * self.elements_per_long) * self.element_bits;
+
+        l >> j & self.max_value
+    }
+
+    /// The backing data of this storage.
+    pub fn data(&self) -> &[u64] {
+        &self.data
+    }
+
+    /// The length of, or the number of elements, in this storage.
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    /// The number of bits each element in this storage uses.
+    pub fn element_bits(&self) -> usize {
+        self.element_bits
+    }
+
+    pub fn for_each<F>(&self, action: F)
+    where
+        F: Fn(u64),
+    {
+        let mut i = 0;
+
+        for l in self.data.iter() {
+            let mut ll = *l;
+            for _ in 0..self.elements_per_long {
+                action(ll & self.max_value);
+
+                ll >>= self.element_bits;
+                i += 1;
+
+                if i >= self.len {
+                    return;
+                }
+            }
+        }
+    }
 }
--- src/world/palette.rs
+++ src/world/palette.rs
@@ -1,11 +1,18 @@
 use crate::network::{Decode, Encode};
 
+/// A palette maps objects from and to small integer IDs that uses less
+/// number of bits to make storage smaller.
+///
+/// While the objects palettes handle are already represented by integer
+/// IDs, shrinking IDs in cases where only a few appear can further reduce
+/// storage space and network traffic volume.
 pub struct Palette<'a, T: 'a> {
     ids: crate::Ref<'a, dyn crate::collections::Indexed<T>>,
     inner: PaletteInner<'a, T>,
 }
 
 impl<'a, T: 'a> Palette<'a, T> {
+    /// Returns the ID of an object in this palette.
     pub fn index(&self, value: &T) -> Option<usize> {
         match &self.inner {
             PaletteInner::Vector(vec) => vec.iter().position(|option| {
@@ -17,6 +24,11 @@ impl<'a, T: 'a> Palette<'a, T> {
         }
     }
 
+    /// Returns the ID of an object in this palette.
+    /// If object does not yet exist in this palette, this palette will
+    /// register the object.
+    ///
+    /// See [`Self::index`].
     pub fn index_or_insert(&mut self, value: &'a T) -> usize {
         self.index(value).unwrap_or_else(|| match &mut self.inner {
             PaletteInner::Vector(vec) => {
@@ -27,16 +39,18 @@ impl<'a, T: 'a> Palette<'a, T> {
         })
     }
 
-    pub fn any<P>(&self, p: P) -> bool
+    /// Returns `true` if any entry in this palette passes the predicate.
+    pub fn any<P>(&self, predicate: P) -> bool
     where
         P: Fn(&T) -> bool,
     {
         match &self.inner {
-            PaletteInner::Vector(vec) => vec.iter().any(|e| e.map_or(false, |ee| p(ee.0))),
+            PaletteInner::Vector(vec) => vec.iter().any(|e| e.map_or(false, |ee| predicate(ee.0))),
             PaletteInner::Indexed(_) => true,
         }
     }
 
+    /// Returns the object associated with the given `index`.
     pub fn get(&self, index: usize) -> Option<&'a T> {
         match &self.inner {
             PaletteInner::Vector(vec) => vec.get(index).map(|e| e.map(|ee| ee.0)).flatten(),
@@ -44,6 +58,8 @@ impl<'a, T: 'a> Palette<'a, T> {
         }
     }
 
+    /// Initializes this palette from the `buf`.
+    /// Clears the preexisting data in this palette.
     pub fn read_buf<B>(&mut self, buf: &mut B) -> anyhow::Result<()>
     where
         B: bytes::Buf,
@@ -67,6 +83,9 @@ impl<'a, T: 'a> Palette<'a, T> {
         Ok(())
     }
 
+    /// Writes this palette to the `buf`.
+    ///
+    /// This is equal with [`Encode::encode`].
     pub fn write_buf<B>(&self, buf: &mut B) -> anyhow::Result<()>
     where
         B: bytes::BufMut,
@@ -95,6 +114,7 @@ impl<'a, T: 'a> Palette<'a, T> {
         Ok(())
     }
 
+    /// The serialized size of this palette in a byte buf, in bytes.
     pub fn buf_len(&self) -> usize {
         match &self.inner {
             PaletteInner::Vector(vec) => {
@@ -120,6 +140,7 @@ impl<'a, T: 'a> Palette<'a, T> {
         }
     }
 
+    /// Size of this palette.
     pub fn len(&self) -> usize {
         match &self.inner {
             PaletteInner::Vector(vec) => vec.len(),
@@ -146,9 +167,12 @@ impl<'a, T: 'a> Clone for Palette<'a, T> {
     }
 }
 
+unsafe impl<'a, T: 'a> Send for Palette<'a, T> {}
+unsafe impl<'a, T: 'a> Sync for Palette<'a, T> {}
+
 enum PaletteInner<'a, T: 'a> {
+    Indexed(crate::Ref<'a, dyn crate::collections::Indexed<T> + Send + Sync>),
     Vector(Vec<Option<crate::Ref<'a, T>>>),
-    Indexed(crate::Ref<'a, dyn crate::collections::Indexed<T>>),
 }
 
 impl<'a, T: 'a> Clone for PaletteInner<'a, T> {
