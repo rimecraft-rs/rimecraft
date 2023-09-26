529262f462fce85fbc28b0edf65ab9210b3cb17b
C191239 <zsz2007314zsz@sina.com>
2023-09-26 17:46:54 +0800

    interface

--- core/src/net/packet.rs
+++ core/src/net/packet.rs
@@ -1,5 +1,16 @@
-pub trait Packet {
-    fn write<B>(&self, buf: &mut B) -> anyhow::Result<()>
-    where
-        B: bytes::BufMut;
+pub trait Packet: super::Encode {
+    type Listener:PacketListener;
+
+    fn apply(&self, listener: Self::Listener) -> anyhow::Result<()>;
+    fn is_writing_err_skippable(&self) -> bool {
+        true
+    }
+}
+
+pub trait PacketListener {
+    const SHOULD_CRASH_ON_EXCEPTION: bool = true;
+
+    ///TODO: Need to implement net.minecraft.Text
+    fn on_disconncted(&self, reason: ()) -> anyhow::Result<()>;
+    fn is_conn_open(&self) -> bool;
 }
