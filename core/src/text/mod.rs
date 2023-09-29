7e062c2b8ab323d42cd0a741045343ec00a8fd37
C191239 <zsz2007314zsz@sina.com>
2023-09-29 22:03:03 +0800

    add todo

--- core/src/text/mod.rs
+++ core/src/text/mod.rs
@@ -1,6 +1,7 @@
+///TODO: Implement net.minecraft.text.Text
 pub trait Text {
     fn style(&self) -> &Style;
 }
 
-//TODO: Implement net.minecraft.text.Style
+///TODO: Implement net.minecraft.text.Style
 pub struct Style;
