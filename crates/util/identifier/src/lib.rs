d738525107f363282485d7b3d86617564615d50e
KrLite <68179735+KrLite@users.noreply.github.com>
2024-03-07 13:00:14 +0800

    identifier wrapper

--- crates/util/identifier/src/lib.rs
+++ crates/util/identifier/src/lib.rs
@@ -17,6 +17,15 @@ pub struct Identifier<N, P> {
     path: P,
 }
 
+/// A generics wrapper for [`Identifier`].
+/// The associated types [`Identifiers::N`] and [`Identifiers::P`] should be applied to [`Identifier`] when used.
+pub trait Identifiers {
+	/// Generic `N` that should be applied to [`Identifier`].
+	type N;
+	/// Generic `P` that should be applied to [`Identifier`].
+	type P;
+}
+
 impl<N, P> Identifier<N, P> {
     /// Creates a new [`Identifier`].
     #[inline]
