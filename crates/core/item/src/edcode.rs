5deeed5e7a8fe6e63dd5b091f89b1dcadb2f0663
C191239 <zsz2007314zsz@sina.com>
2024-03-14 23:38:58 +0800

    partial edcode

--- crates/core/item/src/edcode.rs
+++ crates/core/item/src/edcode.rs
@@ -6,14 +6,20 @@ impl<Cx> Encode for ItemStack<'_, Cx>
 where
     Cx: ItemStackCx,
 {
-    fn encode<B>(&self, buf: B) -> Result<(), std::io::Error>
+    fn encode<B>(&self, mut buf: B) -> Result<(), std::io::Error>
     where
         B: rimecraft_edcode::bytes::BufMut,
     {
         if self.count() == 0 {
             false.encode(buf)?;
         } else {
-            true.encode(buf)?;
+            true.encode(&mut buf)?;
+            let item = self.item();
+            item.encode(&mut buf)?;
+            self.count().encode(buf)?;
+            if item.settings().max_damage.is_some() || item.settings().sync_nbt {
+                todo!()
+            }
         }
         Ok(())
     }
