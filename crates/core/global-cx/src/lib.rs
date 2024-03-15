526a148893557a55100ab94361728e6ef9b891ef
C191239 <zsz2007314zsz@sina.com>
2024-03-15 12:16:42 +0800

    nbt edcode glob cx

--- crates/core/global-cx/src/lib.rs
+++ crates/core/global-cx/src/lib.rs
@@ -8,6 +8,9 @@
 
 #![no_std]
 
+#[cfg(feature = "std")]
+extern crate std;
+
 /// Marker trait for global contexts.
 pub trait GlobalContext: Sized + 'static {}
 
@@ -26,4 +29,65 @@ pub trait ProvideNbtTy: GlobalContext {
     type IntArray;
     /// [`i64`] array type.
     type LongArray;
+}
+
+#[cfg(feature = "std")]
+/// NBT `edcode`-ing related marker traits.
+pub mod nbt_edcode {
+    use crate::ProvideNbtTy;
+
+    /// Marker trait for global contexts that can write nbt tags to a [`std::io::Write`] object.
+    pub trait WriteNbt<T>: ProvideNbtTy {
+        /// Error type.
+        type Error;
+
+        /// Function that performs writing operation.
+        /// # Errors
+        /// I/O errors.
+        fn write_nbt<W>(value: T, writer: W) -> Result<(), Self::Error>
+        where
+            W: std::io::Write;
+    }
+
+    /// Marker trait for global contexts that can read nbt tags from a [`std::io::Read`] object.
+    pub trait ReadNbt<T>: ProvideNbtTy {
+        /// Error type.
+        type Error;
+
+        /// Function that performs reading operation.
+        /// # Errors
+        /// I/O errors.
+        fn read_nbt<R>(reader: R) -> Result<T, Self::Error>
+        where
+            R: std::io::Read;
+    }
+
+    /// Marker trait for global contexts that can update existing nbt tags from a [`std::io::Read`] object.
+    pub trait UpdateNbt<T: ?Sized>: ProvideNbtTy {
+        /// Error type.
+        type Error;
+
+        /// Function that performs updating operation.
+        /// # Errors
+        /// I/O errors.
+        fn update_nbt<R>(value: &mut T, reader: R) -> Result<(), Self::Error>
+        where
+            R: std::io::Read;
+    }
+
+    impl<T, Cx> UpdateNbt<T> for Cx
+    where
+        Cx: ReadNbt<T>,
+    {
+        type Error = <Self as ReadNbt<T>>::Error;
+
+        #[inline]
+        fn update_nbt<R>(value: &mut T, reader: R) -> Result<(), Self::Error>
+        where
+            R: std::io::Read,
+        {
+            *value = Self::read_nbt(reader)?;
+            Ok(())
+        }
+    }
 }
