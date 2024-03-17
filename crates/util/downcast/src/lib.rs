6605b93e0ad0d8dae08d8be950054b9ebe3b9ed1
JieningYu <jiening.yu@outlook.com>
2024-03-17 16:01:15 +0800

    downcast util

---
+++ crates/util/downcast/src/lib.rs
@@ -0,0 +1,117 @@
+//! Downcastable cells and non-static downcasting for Rust.
+
+#![no_std]
+
+use core::{
+    any::TypeId,
+    ops::{Deref, DerefMut},
+};
+
+/// A cell that is able to downcast into a concrete type.
+#[derive(Debug, Clone, Copy)]
+pub struct Downcast<T: ?Sized> {
+    ty: TypeId,
+    value: T,
+}
+
+/// Trait for types that can be converted into a static variant.
+///
+/// # Safety
+///
+/// The repr type should be the static variant of the implemented type.
+pub unsafe trait ToStatic {
+    /// The static variant of this type.
+    type StaticRepr: 'static;
+}
+
+impl<T: ToStatic> Downcast<T> {
+    /// Creates a new `Downcast` cell.
+    #[inline]
+    pub fn new(value: T) -> Self {
+        Self {
+            ty: TypeId::of::<T::StaticRepr>(),
+            value,
+        }
+    }
+}
+
+impl<T> Downcast<T> {
+    /// Creates a new `Downcast` cell with given [`TypeId`].
+    ///
+    /// # Safety
+    ///
+    /// This function could not make sure the type id is correct.
+    #[inline]
+    pub const unsafe fn with_type_id(ty: TypeId, value: T) -> Self {
+        Self { ty, value }
+    }
+}
+
+impl<T> From<T> for Downcast<T>
+where
+    T: ToStatic,
+{
+    #[inline]
+    fn from(value: T) -> Self {
+        Self::new(value)
+    }
+}
+
+impl<T> Default for Downcast<T>
+where
+    T: Default + ToStatic,
+{
+    #[inline]
+    fn default() -> Self {
+        Self::new(Default::default())
+    }
+}
+
+impl<T: ?Sized> Downcast<T> {
+    /// Downcasts the value into a concrete type, returning an immutable reference.
+    #[inline]
+    pub fn downcast_ref<V: ToStatic>(&self) -> Option<&V> {
+        if self.ty == TypeId::of::<V::StaticRepr>() {
+            unsafe { Some(&*(&self.value as *const T as *const V)) }
+        } else {
+            None
+        }
+    }
+
+    /// Downcasts the value into a concrete type, returning a mutable reference.
+    #[inline]
+    pub fn downcast_mut<V: ToStatic>(&mut self) -> Option<&mut V> {
+        if self.ty == TypeId::of::<V::StaticRepr>() {
+            unsafe { Some(&mut *(&mut self.value as *mut T as *mut V)) }
+        } else {
+            None
+        }
+    }
+}
+
+impl<T: ?Sized> Deref for Downcast<T> {
+    type Target = T;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+
+impl<T: ?Sized> DerefMut for Downcast<T> {
+    #[inline]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.value
+    }
+}
+
+/// Safe variant of [`ToStatic`] for implementing [`ToStatic`] on
+/// static types without unsafe code.
+pub trait ToStaticSafe: 'static {}
+
+unsafe impl<T> ToStatic for T
+where
+    T: ToStaticSafe,
+{
+    type StaticRepr = Self;
+}
