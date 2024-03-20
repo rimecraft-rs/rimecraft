cdc38ada4c1d76c7339486e7d670a9f5770c6836
C191239 <zsz2007314zsz@sina.com>
2024-03-20 19:07:50 +0800

    partial

--- crates/util/edcode-macros/src/lib.rs
+++ crates/util/edcode-macros/src/lib.rs
@@ -1,9 +1,33 @@
 //! Proc-macros for deriving [`rimecraft_edcode`] traits.
 
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, Data, DeriveInput};
+
+macro_rules! unsupported_error {
+    ($tr:literal, $ty:literal) => {
+        "deriving `$tr` to `$ty` is not supported"
+    };
+}
 
 /// Derive [`rimecraft_edcode::Encode`] to types.
 #[proc_macro_derive(Encode)]
-pub fn derive_encode(_input: TokenStream) -> TokenStream {
-    TokenStream::new()
+pub fn derive_encode(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    match input.data {
+        Data::Enum(data) => {
+            // TODO: derive `Encode` for `enum`.
+            todo!()
+        }
+        Data::Struct(data) => syn::Error::new(
+            data.struct_token.span,
+            unsupported_error!("Encode", "struct"),
+        )
+        .to_compile_error()
+        .into(),
+        Data::Union(data) => {
+            syn::Error::new(data.union_token.span, unsupported_error!("Encode", "union"))
+                .to_compile_error()
+                .into()
+        }
+    }
 }
