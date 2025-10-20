f36076aac5f0d921a350067734b8bb636f359e7e
KrLite <wang070621@foxmail.com>
2025-10-20 13:17:50 +0800

    update: neatify identifier crate

--- crates/core/resource/src/lib.rs
+++ crates/core/resource/src/lib.rs
@@ -1,1 +1,11 @@
 //! Minecraft resource management.
+
+pub trait ResourceReloader {
+    type Result;
+
+    async fn reload(&self) -> Self::Result;
+}
+
+pub trait ResourceFactory {
+    fn get(&self, id: &str);
+}
