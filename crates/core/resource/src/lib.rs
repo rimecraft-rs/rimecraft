59090baa08477cf9b3b2f298d614deaea1f1d7ec
KrLite <wang070621@foxmail.com>
2025-10-20 13:52:01 +0800

    cleanup

--- crates/core/resource/src/lib.rs
+++ crates/core/resource/src/lib.rs
@@ -1,11 +1,1 @@
 //! Minecraft resource management.
-
-pub trait ResourceReloader {
-    type Result;
-
-    async fn reload(&self) -> Self::Result;
-}
-
-pub trait ResourceFactory {
-    fn get(&self, id: &str);
-}
