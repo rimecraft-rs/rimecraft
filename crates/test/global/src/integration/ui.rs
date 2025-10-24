2e39aaf24a79842c46f5f2ee8628959d2efaa64c
KrLite <wang070621@foxmail.com>
2025-10-24 14:44:10 +0800

    needs redesign

--- crates/test/global/src/integration/ui.rs
+++ crates/test/global/src/integration/ui.rs
@@ -2,29 +2,47 @@
 
 #![cfg(feature = "ui")]
 
-use std::{cell::RefCell, sync::Arc};
+use std::{
+    cell::RefCell,
+    fmt::Debug,
+    sync::{Arc, Mutex, RwLock},
+};
 
 use local_cx::{BaseLocalContext, LocalContext};
 use ui::{Element, ProvideUiTy};
 
 use crate::{TestContext, integration::arena::TestArena};
 
+type Arena<'a> = TestArena<RefCell<Box<dyn Element<'a, TestContext>>>>;
+
 pub struct ArenaLocalContext<'a> {
-    arena: &'a Arc<TestArena<RefCell<Box<dyn Element<'a, TestContext>>>>>,
+    arena: Arc<Arena<'a>>,
+}
+
+impl<'a> ArenaLocalContext<'a> {
+    pub fn new(arena: Arc<Arena<'a>>) -> Self {
+        Self { arena }
+    }
+}
+
+impl Debug for ArenaLocalContext<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("ArenaLocalContext").finish()
+    }
 }
 
 impl BaseLocalContext for &ArenaLocalContext<'_> {}
 
-impl<'g> LocalContext<&TestArena<RefCell<Box<dyn Element<'g, TestContext>>>>>
-    for &ArenaLocalContext<'g>
+impl<'a> LocalContext<&TestArena<RefCell<Box<dyn Element<'a, TestContext>>>>>
+    for &'a ArenaLocalContext<'a>
 {
-    fn acquire(self) -> &'g TestArena<RefCell<Box<dyn Element<'g, TestContext>>>> {
-        self.arena
+    fn acquire(self) -> &'a TestArena<RefCell<Box<dyn Element<'a, TestContext>>>> {
+        &self.arena
     }
 }
 
-impl<'g> ProvideUiTy<'g> for TestContext {
-    type ElementCell = RefCell<Box<dyn Element<'g, Self>>>;
+impl<'a> ProvideUiTy<'a> for TestContext {
+    type ElementCell = RefCell<Box<dyn Element<'a, Self>>>;
     type Arena = TestArena<Self::ElementCell>;
-    type ArenaLocalContext = &'g ArenaLocalContext<'g>;
+    type ArenaLocalContext = &'a ArenaLocalContext<'a>;
 }
--- crates/test/ui/src/lib.rs
+++ crates/test/ui/src/lib.rs
@@ -3,157 +3,195 @@
 #![allow(missing_docs)]
 #![cfg(test)]
 
-// use std::sync::RwLock;
+use std::{
+    cell::RefCell,
+    sync::{Arc, RwLock},
+};
 
-// use test_global::{TestContext, integration::mouse::TestButton};
-// use ui::{
-//     Element, ImmutablyFocusable, ParentElement, ParentElementExt, ParentElementFocusableImpl,
-//     nav::{WithNavIndex, gui::GuiNavigationPath},
-// };
+use arena::Arena;
+use local_cx::LocalContext;
+use test_global::{
+    TestContext,
+    integration::{arena::TestArena, mouse::TestButton, ui::ArenaLocalContext},
+};
+use ui::{
+    Element, Focusable, ParentElement, ParentElementExt, ParentElementFocusableImpl, ProvideUiTy,
+    nav::{WithNavIndex, gui::GuiNavigationPath},
+};
 
-// #[derive(Debug)]
-// struct TestElement {
-//     name: &'static str,
-//     focused: RwLock<bool>,
-// }
+#[derive(Debug)]
+enum TestValue {
+    Number(i32),
+}
 
-// impl TestElement {
-//     const fn new(name: &'static str) -> Self {
-//         Self {
-//             name,
-//             focused: RwLock::new(false),
-//         }
-//     }
-// }
+#[derive(Debug)]
+struct TestElement<'a> {
+    name: &'static str,
+    focused: bool,
+    value: Option<TestValue>,
+    local_cx: &'a ArenaLocalContext<'a>,
+}
 
-// impl WithNavIndex for TestElement {
-//     fn nav_index(&self) -> Option<usize> {
-//         Some(0)
-//     }
-// }
+impl<'a> TestElement<'a> {
+    fn new(name: &'static str, arena: &'a ArenaLocalContext<'a>) -> Self {
+        Self {
+            name,
+            focused: false,
+            value: None,
+            local_cx: arena,
+        }
+    }
+}
 
-// impl ImmutablyFocusable<TestContext> for TestElement {
-//     fn is_focused(&self) -> bool {
-//         let focused = self.focused.read().unwrap();
-//         println!("Is '{}' focused? {}", self.name, *focused);
-//         *focused
-//     }
+impl WithNavIndex for TestElement<'_> {
+    fn nav_index(&self) -> Option<usize> {
+        Some(0)
+    }
+}
 
-//     fn set_focused(&self, focused: bool) {
-//         println!("Setting focus of '{}' to {}", self.name, focused);
-//         let mut guard = self.focused.write().unwrap();
-//         *guard = focused;
-//     }
-// }
+impl Focusable<TestContext> for TestElement<'_> {
+    fn is_focused(&self) -> bool {
+        println!("Is '{:?}' focused? {}", self, self.focused);
+        self.focused
+    }
 
-// impl Element<TestContext> for TestElement {}
+    fn set_focused(&mut self, focused: bool) {
+        println!("Setting focus of '{:?}' to {}", self, focused);
+        self.focused = focused
+    }
+}
 
-// struct TestParentElement<'a> {
-//     name: &'static str,
-//     children: Vec<&'a mut dyn Element<TestContext>>,
-//     dragging_buttons: Vec<TestButton>,
-// }
+impl<'a> Element<'a, TestContext> for TestElement<'a> {}
 
-// impl TestParentElement<'_> {
-//     const fn new(name: &'static str) -> Self {
-//         Self {
-//             name,
-//             children: Vec::new(),
-//             dragging_buttons: Vec::new(),
-//         }
-//     }
-// }
+#[derive(Debug)]
+struct TestParentElement<'a> {
+    name: &'static str,
+    handles: Vec<<<TestContext as ProvideUiTy<'a>>::Arena as Arena>::Handle>,
+    dragging_buttons: Vec<TestButton>,
+    arena: &'a ArenaLocalContext<'a>,
+}
 
-// impl WithNavIndex for TestParentElement<'_> {
-//     fn nav_index(&self) -> Option<usize> {
-//         Some(0)
-//     }
-// }
+impl<'a> TestParentElement<'a> {
+    fn new(name: &'static str, arena: &'a ArenaLocalContext<'a>) -> Self {
+        Self {
+            name,
+            handles: Vec::new(),
+            dragging_buttons: Vec::new(),
+            arena,
+        }
+    }
+}
 
-// impl ParentElementFocusableImpl<TestContext> for TestParentElement<'_> {}
+impl WithNavIndex for TestParentElement<'_> {
+    fn nav_index(&self) -> Option<usize> {
+        Some(0)
+    }
+}
 
-// impl ImmutablyFocusable<TestContext> for TestParentElement<'_> {
-//     fn is_focused(&self) -> bool {
-//         println!(
-//             "Is parent '{}' focused? {}",
-//             self.name,
-//             <Self as ParentElementFocusableImpl<_>>::is_focused(self)
-//         );
-//         <Self as ParentElementFocusableImpl<_>>::is_focused(self)
-//     }
+impl<'a> ParentElementFocusableImpl<'a, TestContext> for TestParentElement<'a> {}
 
-//     fn set_focused(&self, focused: bool) {
-//         println!("Setting focus of parent '{}' to {}", self.name, focused);
-//         <Self as ParentElementFocusableImpl<_>>::set_focused(self, focused);
-//     }
-// }
+impl Focusable<TestContext> for TestParentElement<'_> {
+    fn is_focused(&self) -> bool {
+        println!(
+            "Is parent '{:?}' focused? {}",
+            self,
+            <Self as ParentElementFocusableImpl<_>>::is_focused(self)
+        );
+        <Self as ParentElementFocusableImpl<_>>::is_focused(self)
+    }
 
-// impl ParentElementExt<TestContext> for TestParentElement<'_> {}
+    fn set_focused(&mut self, focused: bool) {
+        println!("Setting focus of parent '{:?}' to {}", self, focused);
+        <Self as ParentElementFocusableImpl<_>>::set_focused(self, focused);
+    }
+}
 
-// impl ParentElement<TestContext> for TestParentElement<'_> {
-//     fn children(&self) -> &[&dyn Element<TestContext>] {}
+impl<'a> ParentElementExt<'a, TestContext> for TestParentElement<'a> {}
 
-//     fn children_mut(&mut self) -> &mut [&mut dyn Element<TestContext>] {}
+impl<'a> ParentElement<'a, TestContext> for TestParentElement<'a> {
+    fn local_cx(&self) -> <TestContext as ProvideUiTy<'a>>::ArenaLocalContext {
+        self.arena
+    }
 
-//     fn dragging_buttons(&self) -> &[TestButton] {
-//         &self.dragging_buttons
-//     }
+    fn handles(&self) -> &[<<TestContext as ProvideUiTy<'a>>::Arena as Arena>::Handle] {
+        &self.handles
+    }
 
-//     fn dragging_buttons_mut(&mut self) -> &mut Vec<TestButton> {
-//         &mut self.dragging_buttons
-//     }
-// }
+    fn handles_mut(
+        &mut self,
+    ) -> &mut Vec<<<TestContext as ProvideUiTy<'a>>::Arena as Arena>::Handle> {
+        &mut self.handles
+    }
 
-// #[test]
-// fn test_instances() {
-//     let element = TestElement::new("leaf");
-//     let mut parent_element = TestParentElement::new("parent");
-//     parent_element.children.push(Box::new(element));
+    fn dragging_buttons(&self) -> &[TestButton] {
+        &self.dragging_buttons
+    }
 
-//     let leaf = GuiNavigationPath::leaf(&element);
-//     let mut node = GuiNavigationPath::node(&parent_element, leaf);
+    fn dragging_buttons_mut(&mut self) -> &mut Vec<TestButton> {
+        &mut self.dragging_buttons
+    }
+}
 
-//     match node {
-//         GuiNavigationPath::Node {
-//             ref mut element,
-//             ref mut child,
-//         } => {
-//             assert!(!element.is_focused());
-//             assert!(!child.is_focused());
+#[test]
+fn test_instances() {
+    let arena = RwLock::new(TestArena::new());
+    let arena_local_cx = ArenaLocalContext::new(Arc::new(arena.read().unwrap()));
 
-//             println!("Focusing element...");
-//             element.focus();
-//             assert!(element.is_focused());
-//             assert!(child.is_focused());
+    let element = TestElement::new("leaf", &arena_local_cx);
+    let element_handle = arena.insert(RefCell::new(Box::new(element)));
 
-//             println!("Setting element unfocused...");
-//             element.set_focused(false);
-//             assert!(!element.is_focused());
-//             assert!(!child.is_focused());
+    let mut parent_element = TestParentElement::new("parent", &arena_local_cx);
+    let parent_element_handle = arena.insert(RefCell::new(Box::new(parent_element)));
 
-//             println!("Focusing child...");
-//             child.focus();
-//             assert!(element.is_focused());
-//             assert!(child.is_focused());
+    parent_element.handles.push(element_handle);
 
-//             println!("Setting child unfocused...");
-//             child.set_focused(false);
-//             assert!(!element.is_focused());
-//             assert!(!child.is_focused());
-//         }
-//         GuiNavigationPath::Leaf(_) => { /* unreachable */ }
-//     }
-// }
+    let leaf = GuiNavigationPath::<'_, TestContext>::leaf(element_handle);
+    let mut node = GuiNavigationPath::<'_, TestContext>::node(parent_element_handle, leaf);
+
+    // match node {
+    //     GuiNavigationPath::Node {
+    //         ref mut element,
+    //         ref mut child,
+    //     } => {
+    //         assert!(!element.is_focused());
+    //         assert!(!child.is_focused());
+
+    //         println!("Focusing element...");
+    //         element.focus();
+    //         assert!(element.is_focused());
+    //         assert!(child.is_focused());
+
+    //         println!("Setting element unfocused...");
+    //         element.set_focused(false);
+    //         assert!(!element.is_focused());
+    //         assert!(!child.is_focused());
+
+    //         println!("Focusing child...");
+    //         child.focus();
+    //         assert!(element.is_focused());
+    //         assert!(child.is_focused());
+
+    //         println!("Setting child unfocused...");
+    //         child.set_focused(false);
+    //         assert!(!element.is_focused());
+    //         assert!(!child.is_focused());
+    //     }
+    //     GuiNavigationPath::Leaf(_) => { /* unreachable */ }
+    // }
+}
 
 // #[test]
 // fn test_navigation() {
-//     let element = TestElement::new("leaf");
-//     let parent_element = TestParentElement::new("parent");
+//     let arena = Arc::new(TestArena::new());
+//     let arena_local_cx = ArenaLocalContext::new(&arena);
+
+//     let element = TestElement::new("leaf", &arena_local_cx);
+//     let parent_element = TestParentElement::new("parent", &arena_local_cx);
 
 //     let leaf = leaf(element);
 //     let parents: Vec<Box<dyn ParentElement<TestContext>>> = vec![
-//         Box::new(TestParentElement::new("parent1")),
-//         Box::new(TestParentElement::new("parent2")),
+//         Box::new(TestParentElement::new("parent1", &arena_local_cx)),
+//         Box::new(TestParentElement::new("parent2", &arena_local_cx)),
 //     ];
 
 //     let path = path(element, parents);
