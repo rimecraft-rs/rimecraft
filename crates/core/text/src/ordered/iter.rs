6c10b9c8c3929ee009f20eb40690bc30c201059b
Jiening Yu <jiening.yu@outlook.com>
2025-11-07 10:08:25 +0800

    primitive iterators for text visiting

--- crates/core/text/src/ordered/iter.rs
+++ crates/core/text/src/ordered/iter.rs
@@ -13,6 +13,8 @@ where
 
 /// Creates an iterator over the characters of a string slice with given [`Style`]s attached.
 ///
+/// The returned iterator is a [`DoubleEndedIterator`] which means you can make it go backwards.
+///
 /// See [`iter_forwards_owned`] for an owned version.
 #[inline]
 pub fn iter_forwards<Cx>(text: &str, style: Style<Cx::StyleExt>) -> IterForwards<'_, Cx>
@@ -22,6 +24,27 @@ where
     IterForwards {
         chars: text.char_indices(),
         style,
+    }
+}
+
+/// Iterator type returned by [`iter_backwards`].
+pub struct IterBackwards<'a, Cx>
+where
+    Cx: ProvideTextTy,
+{
+    inner: IterForwards<'a, Cx>,
+}
+
+/// [`iter_forwards`] but in reverse order.
+///
+/// This is a shorthand for `iter_forwards(text, style).rev()`.
+#[inline]
+pub fn iter_backwards<Cx>(text: &str, style: Style<Cx::StyleExt>) -> IterBackwards<'_, Cx>
+where
+    Cx: ProvideTextTy,
+{
+    IterBackwards {
+        inner: iter_forwards(text, style),
     }
 }
 
@@ -38,6 +61,8 @@ where
 }
 
 /// Creates an iterator over the characters of an owned string with given [`Style`]s attached.
+///
+/// The returned iterator is a [`DoubleEndedIterator`] which means you can make it go backwards.
 pub fn iter_forwards_owned<Cx>(text: String, style: Style<Cx::StyleExt>) -> IterForwardsOwned<Cx>
 where
     Cx: ProvideTextTy,
@@ -47,6 +72,27 @@ where
         // SAFETY: see above.
         inner: iter_forwards(unsafe { &*std::ptr::from_ref(&pinned) }, style),
         _str: pinned,
+    }
+}
+
+/// Iterator type returned by [`iter_backwards_owned`].
+pub struct IterBackwardsOwned<Cx>
+where
+    Cx: ProvideTextTy,
+{
+    inner: IterForwardsOwned<Cx>,
+}
+
+/// [`iter_forwards_owned`] but in reverse order.
+///
+/// This is a shorthand for `iter_forwards_owned(text, style).rev()`.
+#[inline]
+pub fn iter_backwards_owned<Cx>(text: String, style: Style<Cx::StyleExt>) -> IterBackwardsOwned<Cx>
+where
+    Cx: ProvideTextTy,
+{
+    IterBackwardsOwned {
+        inner: iter_forwards_owned(text, style),
     }
 }
 
@@ -92,6 +138,42 @@ where
 {
 }
 
+impl<Cx> Iterator for IterBackwards<'_, Cx>
+where
+    Cx: ProvideTextTy,
+    Cx::StyleExt: Clone,
+{
+    type Item = OrderedTextItem<Cx>;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next_back()
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.inner.size_hint()
+    }
+}
+
+impl<Cx> DoubleEndedIterator for IterBackwards<'_, Cx>
+where
+    Cx: ProvideTextTy,
+    Cx::StyleExt: Clone,
+{
+    #[inline]
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.inner.next()
+    }
+}
+
+impl<Cx> FusedIterator for IterBackwards<'_, Cx>
+where
+    Cx: ProvideTextTy,
+    Cx::StyleExt: Clone,
+{
+}
+
 impl<Cx> Iterator for IterForwardsOwned<Cx>
 where
     Cx: ProvideTextTy,
@@ -128,6 +210,42 @@ where
 {
 }
 
+impl<Cx> Iterator for IterBackwardsOwned<Cx>
+where
+    Cx: ProvideTextTy,
+    Cx::StyleExt: Clone,
+{
+    type Item = OrderedTextItem<Cx>;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next_back()
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.inner.size_hint()
+    }
+}
+
+impl<Cx> DoubleEndedIterator for IterBackwardsOwned<Cx>
+where
+    Cx: ProvideTextTy,
+    Cx::StyleExt: Clone,
+{
+    #[inline]
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.inner.next()
+    }
+}
+
+impl<Cx> FusedIterator for IterBackwardsOwned<Cx>
+where
+    Cx: ProvideTextTy,
+    Cx::StyleExt: Clone,
+{
+}
+
 impl<Cx> Clone for IterForwards<'_, Cx>
 where
     Cx: ProvideTextTy,
@@ -155,6 +273,31 @@ where
     }
 }
 
+impl<Cx> Clone for IterBackwards<'_, Cx>
+where
+    Cx: ProvideTextTy,
+    Cx::StyleExt: Clone,
+{
+    #[inline]
+    fn clone(&self) -> Self {
+        Self {
+            inner: self.inner.clone(),
+        }
+    }
+}
+
+impl<Cx> Debug for IterBackwards<'_, Cx>
+where
+    Cx: ProvideTextTy,
+    Cx::StyleExt: Debug,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("IterBackwards")
+            .field("inner", &self.inner)
+            .finish()
+    }
+}
+
 impl<Cx> Clone for IterForwardsOwned<Cx>
 where
     Cx: ProvideTextTy,
@@ -175,8 +318,33 @@ where
     Cx::StyleExt: Debug,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_tuple("IterForwardsOwned")
-            .field(&self.inner)
+        f.debug_struct("IterForwardsOwned")
+            .field("inner", &self.inner)
             .finish_non_exhaustive()
+    }
+}
+
+impl<Cx> Clone for IterBackwardsOwned<Cx>
+where
+    Cx: ProvideTextTy,
+    Cx::StyleExt: Clone,
+{
+    #[inline]
+    fn clone(&self) -> Self {
+        Self {
+            inner: self.inner.clone(),
+        }
+    }
+}
+
+impl<Cx> Debug for IterBackwardsOwned<Cx>
+where
+    Cx: ProvideTextTy,
+    Cx::StyleExt: Debug,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("IterBackwardsOwned")
+            .field("inner", &self.inner)
+            .finish()
     }
 }
