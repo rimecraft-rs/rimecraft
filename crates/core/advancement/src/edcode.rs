13eba168f41277f8ca945920ddf7ba957b13e796
C191239 <zsz2007314zsz@sina.com>
2026-01-13 17:42:28 +0800

    myth error

--- crates/core/advancement/src/edcode.rs
+++ crates/core/advancement/src/edcode.rs
@@ -4,8 +4,8 @@ use rimecraft_global_cx::{
     nbt::{ReadNbt, WriteNbt},
 };
 use rimecraft_item::{component::RawErasedComponentType, ItemStack, RawItem};
-use rimecraft_local_cx::{dyn_cx::AsDynamicContext, LocalContext, WithLocalCx};
-use rimecraft_registry::{ProvideRegistry, Registry};
+use rimecraft_local_cx::{LocalContext, WithLocalCx};
+use rimecraft_registry::Registry;
 use rimecraft_text::Text;
 
 use crate::{AdvancementCx, DisplayInfo, Frame};
@@ -16,18 +16,17 @@ pub trait AdvancementEdcodeCx: AdvancementCx {
     fn frame_fmt(name: &str) -> Frame;
 }
 
-impl<'r, Cx, B, L> Encode<WithLocalCx<B, L>> for DisplayInfo<'r, Cx>
+impl<'r, Cx, B> Encode<WithLocalCx<B, Cx::LocalContext<'_>>> for DisplayInfo<'r, Cx>
 where
     Cx: AdvancementEdcodeCx
         + LocalContext<Registry<Cx::Id, RawItem<'r, Cx>>>
         + for<'a, 'b> WriteNbt<&'a &'b Text<Cx>>,
     B: BufMut,
-    L: AsDynamicContext,
     Cx::Id: for<'a> Encode<&'a mut B>,
 {
     fn encode(
         &self,
-        mut buf: WithLocalCx<B, L>,
+        mut buf: WithLocalCx<B, Cx::LocalContext<'_>>,
     ) -> Result<(), rimecraft_edcode2::BoxedError<'static>> {
         Nbt::<&Text<Cx>, Cx>::new(&self.title).encode(&mut buf)?;
         Nbt::<&Text<Cx>, Cx>::new(&self.description).encode(&mut buf)?;
@@ -56,8 +55,8 @@ where
 impl<'de, 'r, Cx, B> Decode<'de, B> for DisplayInfo<'r, Cx>
 where
     Cx: AdvancementEdcodeCx
-        + ProvideRegistry<'r, Cx::Id, RawItem<'r, Cx>>
-        + ProvideRegistry<'r, Cx::Id, RawErasedComponentType<'r, Cx>>
+        + LocalContext<&'r Registry<Cx::Id, RawItem<'r, Cx>>>
+        + LocalContext<&'r Registry<Cx::Id, RawErasedComponentType<'r, Cx>>>
         + ReadNbt<Text<Cx>>,
     B: Buf,
     Cx::Id: for<'a, 'b> Decode<'de, &'a mut &'b mut B> + for<'a> Decode<'de, &'a mut B>,
